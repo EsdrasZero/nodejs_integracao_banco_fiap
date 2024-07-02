0bc70da26681a089d521c48f7e515348b8bfe1a9
Esdras Mendes <esdrasmendes0@outlook.com>
2024-07-02 16:53:04 -0300

    update dos scripts build/dev/start

--- pettech/src/server.ts
+++ pettech/src/server.ts
@@ -8,5 +8,5 @@ app
     port: env.PORT,
   })
   .then(() => {
-    console.log("Server is running on http://localhost:3000");
+    console.log(`Server is running on http://localhost:${env.PORT}`);
   });
